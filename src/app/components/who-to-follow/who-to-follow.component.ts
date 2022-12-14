6ba876d184badf5e6d0a07f780522a207eb4d841
Tyler Tierney <tytierney@yahoo.com>
2022-12-13 23:38:46 -0500

    More things

--- src/app/components/who-to-follow/who-to-follow.component.ts
+++ src/app/components/who-to-follow/who-to-follow.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   selector: 'app-who-to-follow',
   templateUrl: './who-to-follow.component.html',
-  styleUrls: ['./who-to-follow.component.css']
+  styleUrls: ['./who-to-follow.component.css'],
 })
 export class WhoToFollowComponent implements OnInit {
+  allUsers$: Observable<any[]>;
 
-  constructor() { }
+  constructor(private usersService: UserService) {}
 
   ngOnInit(): void {
+    this.allUsers$ = this.usersService.getAllUsers();
   }
-
 }
