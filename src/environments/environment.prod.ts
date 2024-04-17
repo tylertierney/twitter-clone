9a35a71c7bc6efc44f3d323d73cf639cf3559171
Tyler Tierney <tytierney@yahoo.com>
2024-04-17 14:37:38 -0400

    trying railway.app for api deployments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  domain: 'https://twitter-backend-2te8.onrender.com/api',
+  domain: 'https://twitter-clone-backend-production-cd1f.up.railway.app/api',
+  // domain: 'https://twitter-backend-2te8.onrender.com/api',
   // domain: 'http://localhost:8080/api',
   // domain: 'http://192.168.254.32:8080/api',
 };
